e8512982d8e7ca4971c4cef9f82ce983411e4478
Md Fakhruddin Ferdous <ferdous.frd81@gmail.com>
2021-02-13 19:25:21 +0200

    Modified it coz wanted to use nested if else in it
    Just going through old high school C++ stuff and sharping my skills xD

--- Salary calculation.cpp
+++ Salary calculation.cpp
@@ -1,21 +1,33 @@
+//Using if else statement in my code :D
 #include <iostream>
 using namespace std;
 
-//simple monthly/yearly salary counter
+//Modifying my old simple monthly/yearly salary counter 
+//Using nested if else statments
+//Just toying with it lol :'D
 
 int main() {
 	float monthlySalary;
 
 	cout << "Input your monthly salary ";
 	cin >> monthlySalary;
-	cout << "Monthly you get : "  << monthlySalary << endl;
+	cout << "Monthly you get : " << monthlySalary << endl;
 
-	int months;
-	cout << "Enter the number of how many months total salary you wana know" << endl;
-	cin >> months;
-	float usersWishedSalary = monthlySalary * months;
-	cout << "Your output is : " << usersWishedSalary << endl;
+	float annualSalary = monthlySalary * 12; //annual or yearly salary = monthlySalary*1year(12months)
+	cout << "Your Annualsalary is : " << annualSalary << endl;
+
+	if (annualSalary >= 15000) 
+		cout << "You are doing great. Be my Suger daddy lol :p " << endl;
 	
+	else {
+		if (annualSalary >= 7000) 
+			cout << "You gotta work hard " << endl;		
+		else 
+			cout << "You are in bad shape!! you need to hustle more. just keep hustling. You can do it!" << endl;
+	}
+		
+
+
 	system("pause>0");
 
 }
